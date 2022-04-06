d4ce928c3fe9182fc5cf686a6f3bd014778398b4
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-04-07 00:42:42 +0900

    get data from db

--- app/Controllers/Tasks.php
+++ app/Controllers/Tasks.php
@@ -7,11 +7,10 @@ class Tasks extends BaseController
     public function index()
     {
 
-        $data = [
-            ['id' => 1, "description" => "First task"],
-            ['id' => 2, "description" => "Second task"],
-        ];
+        $model = new \App\Models\TaskModel;
 
+
+        $data = $model->findAll();
 
         return view("Tasks/index", ["tasks" => $data]);
     }
