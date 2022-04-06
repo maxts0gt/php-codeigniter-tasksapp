b7a97416282b2dbfd6a2a0645cb5ba386aa90bb7
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-04-06 23:56:47 +0900

    show todo list on tasks

--- app/Controllers/Tasks.php
+++ app/Controllers/Tasks.php
@@ -7,6 +7,12 @@ class Tasks extends BaseController
     public function index()
     {
 
-        return view("Tasks/index");
+        $data = [
+            ['id' => 1, "description" => "First task"],
+            ['id' => 2, "description" => "Second task"],
+        ];
+
+
+        return view("Tasks/index", ["tasks" => $data]);
     }
 }
--- app/Views/Tasks/index.php
+++ app/Views/Tasks/index.php
@@ -10,4 +10,20 @@
 
 <h1>Tasks page</h1>
 
+
+<ul>
+
+    <?php foreach ($tasks as $task) : ?>
+
+        <li>
+            <?= $task['id'] ?>
+            <?= $task['description'] ?>
+        </li>
+
+    <?php endforeach; ?>
+
+</ul>
+
+
+
 <?= $this->endSection() ?>
